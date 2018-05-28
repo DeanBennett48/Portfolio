060c9726a78b532ac36b56aae325f86c9c6c6b79
Dean <deanbennett48@gmail.com>
2018-05-28 17:12:47 +0100

    AAA

--- Scripts/js.js
+++ Scripts/js.js
@@ -1,1 +1,15 @@
+window.addEventListener("scroll", checkPagePos);
 
+function checkPagePos() {
+    let nav = document.getElementById('nav');
+    if(window.scrollY>100) {
+        if(nav.classList.contains('fixed-nav')===false){
+            nav.classList.toggle('fixed-nav');
+        }
+    }
+    else {
+        if(nav.classList.contains('fixed-nav')) {
+            nav.classList.toggle('fixed-nav');
+        }     
+    }
+}
